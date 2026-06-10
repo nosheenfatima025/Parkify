66961fc25fd1b956fa6a186ebff9c304dd0f41d0
nosheenfatima025 <nosheenfatima025@gmail.com>
2026-06-10 15:13:38 +0500

    Fix vehicle routes security (adminOnly added)

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,11 +1,16 @@
-
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { addVehicle, getAllVehicles, getMyVehicles, deactivateVehicle } = require("../controllers/vehicleController");
+
+const { protect, adminOnly } = require("../middleware/authMiddleware");
+const {
+    addVehicle,
+    getAllVehicles,
+    getMyVehicles,
+    deactivateVehicle
+} = require("../controllers/vehicleController");
 
 router.post("/add", protect, addVehicle);
-router.get("/all", getAllVehicles);
+router.get("/all", protect, adminOnly, getAllVehicles);
 router.get("/my", protect, getMyVehicles);
 router.put("/:id/deactivate", protect, deactivateVehicle);
 
